bcf9804fa437beb829e684c70bbefeab2f1c1cc8
Sudeep Basnet <sudbasnet10@gmail.com>
2018-10-17 20:34:57 -0500

    Converted Multigrid to Hexagon grid. Changed agent model, removed the GridCell agents, penguins now look for the nearest other penguin that has a temprature higher than it’s own, if no other penguin found then try to maximize its neighborhood.

--- penguin_huddle/agent.py
+++ penguin_huddle/agent.py
@@ -1,45 +1,122 @@
 from mesa import Agent
 from model import *
-HEAT_LOSS_GROUND = 0.01
-WHEN_DO_PENGUINS_DIE = 5.0
+from math import sqrt, pow, inf
 
 
 class Penguin(Agent):
-    def __init__(self, unique_id, model, heat, loss_rate, open_sides):
+    def __init__(self, unique_id, model, heat, loss_rate):
         super().__init__(unique_id, model)
         self.heat = heat
         self.loss_rate = loss_rate
-        self.open_sides = open_sides
+        # self.open_sides = open_sides
         self.kill = False
+        self.waddle_id = 0
+        self.open_sides = []
+        self.open_positions = []
+        self.last_position = None
 
     def lose(self):
-        global HEAT_LOSS_GROUND
-        self.heat = self.heat - (self.heat * ((self.loss_rate * self.open_sides) + self.model.ground_loss_rate))
+        losing = self.heat * ((self.loss_rate * self.open_sides) + self.model.ground_loss_rate)
+        self.heat = self.heat - losing
+        # cell_contains = self.model.grid.get_cell_list_contents([self.pos])
+        # for gridcell in cell_contains:
+        #     if type(gridcell) is GridCell:
+        #         gridcell.heat += (0.9 * losing)  # 90 % of what the penguin looses is given to the grid
 
     def live_or_die(self):
         if self.heat <= self.model.die_temperature:
             self.kill = True
 
-    def step(self):
-        self.lose()
-        self.live_or_die()
-        # print("unique_id and heat level: ", self.unique_id, self.heat)
+    def set_waddle_id(self, w):
+        self.waddle_id = w
 
+    def check_openness(self):
+        # Check if the penguin is open in four directions: East (E), North (N), West (W), South (S)
+        neighborhood = self.model.grid.get_neighborhood(self.pos)
+        self.open_sides = 0
+        self.open_positions = []
+        for n in neighborhood:
+            if self.model.grid.is_cell_empty(n):
+                self.open_positions.append(n)
+                self.open_sides += 1
 
-class GridCell(Agent):
-    def __init__(self, unique_id, model, heat):
-        super().__init__(unique_id, model)
-        self.heat = heat
-        self.occupied = False
+    # looks around and finds the grid that the penguin wants to go to
+    def find_lighthouse(self):
+        self.check_openness()
+        walk_length = inf
+        lighthouse = self.pos
+        neighbors = self.model.grid.get_neighbors(self.pos)
+        for x in range(self.model.grid.width):
+            for y in range(self.model.grid.height):
+                for hot_penguin in self.model.grid.get_cell_list_contents([(x, y)]):
+                    if (x, y) != self.pos and hot_penguin.heat >= self.heat and hot_penguin not in neighbors:
+                        dist = euclidean(self.pos, hot_penguin.pos)
+                        if dist < walk_length:
+                            walk_length = dist
+                            lighthouse = hot_penguin.pos
+        return lighthouse
 
-    def occupancy(self):
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        if len(cellmates) > 1:
-            self.occupied = True
+    def next_pos(self):
+        next_pos = self.pos
+        lighthouse = self.find_lighthouse()
+        self.check_openness()
+        if lighthouse == self.pos:
+            neighbor_count = len(self.model.grid.get_neighbors(self.pos))
+            new_neighbor_count = neighbor_count
+            for op in self.open_positions:
+                if len(self.model.grid.get_neighbors(op))-1 > new_neighbor_count and op != self.last_position:
+                    new_neighbor_count = len(self.model.grid.get_neighbors(op))-1
+                    next_pos = op
         else:
-            self.occupied = False
+            best_distance = inf
+            for np in self.open_positions:
+                if euclidean(np, lighthouse) < best_distance and np != self.last_position:
+                    best_distance = euclidean(np, lighthouse)
+                    next_pos = np
+        return next_pos
+
+    # def make_waddle(self):
+    #     for p in self.model.grid.get_neighbors(self):
+    #         if p.waddle_id < self.waddle_id:
+    #             self.waddle_id = p.waddle_id
 
     def step(self):
-        self.occupancy()
-        # if self.occupied:
-        #     print(self.pos, " occupado")
+        if type(self) is Penguin and self.pos is not None:
+            new_position = self.next_pos()
+            self.last_position = self.pos
+            self.model.grid.move_agent(self, new_position)
+            # self.make_waddle()
+            self.lose()
+            self.live_or_die()
+
+
+# class GridCell(Agent):
+#     def __init__(self, unique_id, model, heat, loss_rate):
+#         super().__init__(unique_id, model)
+#         self.heat = heat
+#         self.occupied = False
+#         self.loss_rate = loss_rate
+#
+#     def occupancy(self):
+#         cellmates = self.model.grid.get_cell_list_contents([self.pos])
+#         if len(cellmates) > 1:
+#             self.occupied = True
+#         else:
+#             self.occupied = False
+#
+#     def is_occupied(self):
+#         return self.occupied
+#
+#     def lose(self):
+#         self.heat = max(self.heat - (self.heat * self.loss_rate), 0)
+#
+#     def step(self):
+#         self.occupancy()
+#         self.lose()
+#         # if self.occupied:
+#         #     print(self.pos, " occupado")
+
+
+# simply define the euclidean distance between point a and b
+def euclidean(a, b):
+    return sqrt(pow(a[0]-b[0], 2) + pow(a[1]-b[1], 2))
--- penguin_huddle/model.py
+++ penguin_huddle/model.py
@@ -1,8 +1,9 @@
 from agent import *
+# import queue as q
 from mesa import Model
 from mesa.time import RandomActivation
 import random
-from mesa.space import MultiGrid
+from mesa.space import HexGrid
 from mesa.datacollection import DataCollector
 
 
@@ -11,7 +12,7 @@ class Arctic(Model):
         self.num_agents = N
 
         # false means he grid is not toroidal
-        self.grid = MultiGrid(width, height, False)
+        self.grid = HexGrid(width, height, False)
 
         self.schedule = RandomActivation(self)
         self.temperature = temperature
@@ -21,18 +22,19 @@ class Arctic(Model):
         self.die_temperature = die_temperature
 
         # Create the GridCell agents for each cell
-        for x in range(self.grid.width):
-            for y in range(self.grid.height):
-                c = GridCell(unique_id=(x, y), model=self, heat=self.temperature)
-                self.schedule.add(c)
-                self.grid.place_agent(c, (x, y))
+        # for x in range(self.grid.width):
+        #     for y in range(self.grid.height):
+        #         c = GridCell(unique_id=(x, y), model=self, heat=self.temperature, loss_rate=0.1)
+        #         self.schedule.add(c)
+        #         self.grid.place_agent(c, (x, y))
 
         # Place the penguins in random grid locations
         # Penguins should not overlap
         # Create a list of locations where the penguins are already assigned
         occupied = []
+        waddle_id = 1
         for i in range(self.num_agents):
-            p = Penguin(unique_id=i, model=self, heat=40.0, loss_rate=0.0, open_sides=4)
+            p = Penguin(unique_id=i, model=self, heat=40.0, loss_rate=0.0)
             self.schedule.add(p)
             if len(occupied) >= (self.grid.width * self.grid.height):
                 break
@@ -41,22 +43,33 @@ class Arctic(Model):
             while (x, y) in occupied:
                 x = random.randrange(self.grid.width)
                 y = random.randrange(self.grid.height)
-            occupied.append((x,y))
+            occupied.append((x, y))
             self.grid.place_agent(p, (x, y))
-            grid_cellmate = self.grid.get_cell_list_contents((x,y))
-            for j in grid_cellmate:
-                if type(j) is GridCell:
-                    j.occupancy()
+            p.check_openness()
+            neighbors = self.grid.get_neighbors((x, y))
+            if len(neighbors) > 0:
+                waddle_id = neighbors[0].waddle_id
+            else:
+                waddle_id += 1
+            p.set_waddle_id(waddle_id)
+
+            # grid_cellmate = self.grid.get_cell_list_contents((x,y))
+            # for j in grid_cellmate:
+            #     if type(j) is GridCell:
+            #         j.occupancy()
 
         self.data_collector = DataCollector(
             model_reporters={"Average Heat of Penguins": average_heat}
         )
 
+        self.waddles = {}
+
     def remove_ghosts(self):
         for x in range(self.grid.width):
             for y in range(self.grid.height):
                 for agent in self.grid.get_cell_list_contents([(x, y)]):
-                    if type(agent) is Penguin and agent.kill:
+                    # if type(agent) is Penguin and agent.kill:
+                    if agent.kill:
                         self.grid.remove_agent(agent)
 
     def kill_penguin(self, agent):
@@ -71,6 +84,26 @@ class Arctic(Model):
 def average_heat(model):
     agent_heats = []
     for agent in model.schedule.agents:
-        if type(agent) is Penguin:
-            agent_heats.append(agent.heat)
+        # if type(agent) is Penguin:
+        agent_heats.append(agent.heat)
     return round(sum(agent_heats)/len(agent_heats), 2)
+
+
+# def get_waddle_number(model):
+#     for x in model.grid.width:
+#         for y in model.grid.height:
+#             agents = model.grid.get_cell_list_contents([(x, y)])
+#             if len(agents) > 0:
+#                 sd
+# def make_waddle(model):
+#     waddle_id = 1
+#     visited = []
+#     for x in range(model.grid.width):
+#         for y in range(model.grid.height):
+#             for agent in model.grid.get_cell_list_contents([(x, y)]):
+#                 if type(agent) is Penguin and agent.unique_id not in visited:
+#                     visited.append(agent.unique_id)
+#                     others = model.grid.get_neighbors((x, y), moore=True)
+#                     # for
+#                     # model.grid.remove_agent(agent)
+
